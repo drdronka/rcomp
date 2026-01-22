5fa7f022d6d7ecf96cd66d4c870c4231531dd083
drdronka <dronka@runbox.com>
2026-01-22 04:19:09 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::fs;
+use std::env;
+//use log::debug;
 
-const DEBUG_READ_PRINT: bool = false;
-const DEBUG_STATS_PRINT: bool = true;
+const VERBOSE_EN: bool = true;
 
 struct Stats {
     byte: [u8; 256],
@@ -10,44 +11,71 @@ struct Huff {
     stats: Option<Stats>,
 }
 
-impl Huff {
-    fn calc_stats_from_file(&mut self, path_in: &str) {
-        println!("reading file: {path_in}");
-        let data = match fs::read(path_in) {
-            Ok(data) => {
-                println!("read ok");
-                data
-            }
-            Err(_) => {
-                println!("read failed");
-                return;
-            }
-        };
-        if DEBUG_READ_PRINT { 
-            println!("data read: {:?}", data); 
-        }
-        self.stats = Some(self.calc_stats_from_data(&data));
+impl Stats {
+    fn from_file(path_in: &str) -> Result<Stats, std::io::Error> {
+        let data = fs::read(path_in)?;
+        return Ok(Stats::from_data(&data));
     }
 
-    fn calc_stats_from_data(&self, data: &Vec<u8>) -> Stats {
+    fn from_data(data: &Vec<u8>) -> Stats {
         let mut stats = Stats { byte: [0; 256] };
         for val in data.iter() {
             stats.byte[*val as usize] += 1;
         }
-        if DEBUG_STATS_PRINT {
-            println!("data stats:");
-            for (idx, elem) in stats.byte.iter().enumerate() {
-                if *elem > 0 {
-                    println!("0x{:02X}: {}", idx, *elem);
-                }
-            }
-        }
         stats
+    }
+
+    fn print(&self) {
+        println!("stats:");
+        for (idx, elem) in self.byte.iter().enumerate() {
+            if *elem > 0 {
+                println!("{:02X}: {}", idx, *elem);
+            }
+        } 
     }
 }
 
+impl Huff {
+    fn calc_tree(&self) {
+    }
+}
+
+fn compress(path_in: &str, path_out: &str, verbose: bool) {
+    println!("starting compression");
+    println!("input file: {}", path_in);
+    println!("output file: {}", path_out);
+
+    print!("calculating stats.. ");
+    let stats: Stats = match Stats::from_file(path_in) {
+        Ok(stats) => {
+            println!("done");
+            stats
+        },
+        Err(msg) => {
+            println!("failed: {}", msg);
+            return;
+        },
+    };
+
+    if verbose {
+        stats.print();
+    }
+
+    println!("calculating huffman tree");
+}
+
 fn main() {
-    let path_test_in = "test_in";
     let mut huff = Huff { stats: None };
-    huff.calc_stats_from_file(path_test_in);
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() <= 2 {
+        println!("usage: {} [src_file] [dst_file]", args[0].split('/').last().unwrap());
+        return;
+    }
+
+    compress(args[1].as_str(), args[2].as_str(), VERBOSE_EN);
+
+//    let mut asdf = vec![0, 1, 2, 3, 4];
+//    asdf.remove(2);    
+//    println!("{:?}", asdf);
 }
